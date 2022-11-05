420ddc695c42be63e51036a77f93adb63feeb85c
chirag ramani <ramanichirag069@gmail.com>
2022-11-05 10:41:02 +0530

    splash screen change

--- app/Navigation/Index.js
+++ app/Navigation/Index.js
@@ -5,13 +5,21 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useSelector, useDispatch} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {LoggedAction} from '../redux/actions/authActons';
+import SplashScreen from 'react-native-splash-screen';
 
 const Index = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     (async () => {
       const userToken = await AsyncStorage.getItem('@user_token');
-      dispatch(LoggedAction(userToken));
+      try {
+        dispatch(LoggedAction(userToken));
+        setTimeout(() => {
+          SplashScreen.hide();
+        }, 1000);
+      } catch (e) {
+        SplashScreen.hide();
+      }
     })();
   }, [dispatch]);
 
